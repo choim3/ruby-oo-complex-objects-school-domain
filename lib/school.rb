7a9d0ccc72e3d268cb031974d6c62b984e32c10f
choim3 <choim3@spu.edu>
2020-11-18 15:34:51 -0800

    commit tree 3622c2bb61b931658a3796921bd1ee91e3ab4877

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,55 @@
-# code here!
+require 'pry'
+class School
+    attr_reader :school_name
+    attr_accessor :roster
+  
+    def initialize(school_name)
+        @school_name = school_name
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        @roster[grade] ||= []
+        @roster[grade] << name
+    end
+
+    def grade(school_grade)
+        roster.each do |key, value|
+            if key == school_grade
+                return value
+            end
+        end
+    end
+
+    def sort
+        roster.each do |grade_level, names|
+            binding.pry 
+            roster[grade_level] = names.sort
+        end
+        return roster
+    end
+    
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#     grade_arr = roster.select {|value| roster[grade] == school_grade}
+    #     grade_arr
+    # end
